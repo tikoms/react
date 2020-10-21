73af0388e5cdf060a557786a4c84ee6bfbf19d3a
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-10-21 01:17:48 +0100

    constructor function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+/*
+const student1 = {
+    id: 1,
+    name: "Reed",
+    subjects: [],
+    addSubject(subject) {
+        this.subjects = [...this.subjects, subject];
+    },
+};
+*/
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+function Student(id, name, subjects = []) {
+    this.id = id;
+    this.name = name;
+    this.subjects = subjects;
+}
+
+Student.prototype.addSubject = function(subject) {
+    this.subjects = [...this.subjects, subject];
+};
+
+const student1 = new Student(1, "Reed");
+console.log(student1);
+
+const student2 = new Student(2, "Gregory");
+console.log(student2);
+
+student1.addSubject("Astrophysics");
+student2.addSubject("Chemistry");
+
+console.log(student1);
+console.log(student2);
+
+/*
+console.log(student1);
+student1.addSubject("Math");
+console.log(student1);
+*/
