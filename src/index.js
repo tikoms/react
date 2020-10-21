d134b5959192d0d36b9e4d82b77316e171fcba9e
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-10-21 02:51:26 +0100

    constructor function- challenge

--- src/index.js
+++ src/index.js
@@ -1,38 +1,19 @@
-/*
-const student1 = {
-    id: 1,
-    name: "Reed",
-    subjects: [],
-    addSubject(subject) {
-        this.subjects = [...this.subjects, subject];
-    },
-};
-*/
-
-function Student(id, name, subjects = []) {
+function Book(id, title, author, themes = []) {
     this.id = id;
-    this.name = name;
-    this.subjects = subjects;
+    this.title = title;
+    this.author = author;
+    this.themes = themes;
 }
 
-Student.prototype.addSubject = function(subject) {
-    this.subjects = [...this.subjects, subject];
+const book1 = new Book(1, "Passport of Malam Ilia", "Chinua Achebe");
+const book2 = new Book(2, "Things fall apart", "Chinua Achebe");
+
+Book.prototype.addTheme = function(theme) {
+    this.themes = [...this.themes, theme];
 };
 
-const student1 = new Student(1, "Reed");
-console.log(student1);
+book1.addTheme("La Isla Bonita");
+book2.addTheme("Senzana Donner");
 
-const student2 = new Student(2, "Gregory");
-console.log(student2);
-
-student1.addSubject("Astrophysics");
-student2.addSubject("Chemistry");
-
-console.log(student1);
-console.log(student2);
-
-/*
-console.log(student1);
-student1.addSubject("Math");
-console.log(student1);
-*/
+console.log(book1);
+console.log(book2);
